f376752de584f74fe9557c7db03597a8552a001e
krfix <mkarwala22@gmail.com>
2023-10-15 20:16:47 +0200

    fetcher is now only metadate reader and json build

--- fetcher.py
+++ fetcher.py
@@ -1,8 +1,6 @@
 import os
 import exifread
 import json
-from PIL import Image
-import rawpy
 
 # Input and output directories
 feed_folder = "source"
@@ -28,46 +26,10 @@ desired_tags = [
     'Image Make',
 ]
 
-# Define the new dimensions for the original image
-new_width = 3000
-new_height = 2000
-
 # Iterate through the files in the feed folder
 for filename in os.listdir(feed_folder):
     if filename.endswith(('.jpg', '.jpeg', '.png', '.bmp', '.gif', '.ARW')):
         try:
-            # Check if the file is an ARW file
-            is_arw = filename.endswith('.ARW')
-
-            if is_arw:
-                # Open and process the ARW file using rawpy
-                with rawpy.imread(os.path.join(feed_folder, filename)) as raw:
-                    # Extract the RGB image
-                    rgb = raw.postprocess()
-
-                image = Image.fromarray(rgb)
-            else:
-                # Open other image formats using PIL
-                image = Image.open(os.path.join(feed_folder, filename))
-
-            # Save the original image as webp in the gallery folder with new dimensions
-            gallery_filename = os.path.splitext(filename)[0] + '.webp'
-            image = image.resize((new_width, new_height), Image.BILINEAR)
-            image.save(os.path.join(gallery_folder, gallery_filename), 'webp')
-
-            # Calculate the new size (420x280) for the thumbnail
-            thumbnail_width = 420
-            thumbnail_height = 280
-
-            # Resize the image using the BILINEAR filter
-            resized_image = image.resize((thumbnail_width, thumbnail_height), Image.BILINEAR)
-
-            # Create the thumbnail file name
-            thumbnail_filename = os.path.splitext(filename)[0] + '.webp'
-
-            # Save the resized image as webp in the thumbnails folder
-            resized_image.save(os.path.join(thumbnails_folder, thumbnail_filename), 'webp')
-
             # Extract metadata
             arw_file_path = os.path.join(feed_folder, filename)
             with open(arw_file_path, 'rb') as arw_file:
@@ -106,13 +68,14 @@ for filename in os.listdir(feed_folder):
                             metadata[tag] = str(tags[tag])
 
                 # Append the gallery and thumbnail paths to the respective lists
+                gallery_filename = os.path.splitext(filename)[0] + '.jpg'
                 image_data.append({
                     "src": f"./{gallery_folder}/{gallery_filename}",
-                    "thumbnailSrc": f"./{thumbnails_folder}/{thumbnail_filename}",
+                    "thumbnailSrc": f"./{thumbnails_folder}/{gallery_filename}",
                     "info": metadata
                 })
 
-                print(f"Resized and saved: {thumbnail_filename}")
+                print(f"Metadata extracted for: {filename}")
         except Exception as e:
             print(f"Error processing {filename}: {str(e)}")
 
@@ -120,5 +83,4 @@ for filename in os.listdir(feed_folder):
 with open(fetchlist_path, 'w') as fetchlist_file:
     json.dump({"images": image_data}, fetchlist_file, indent=4)
 
-print("All images saved as webp in gallery and as thumbnails in thumbnails folder.")
 print("Metadata extracted and saved to fetchlist.json.")
